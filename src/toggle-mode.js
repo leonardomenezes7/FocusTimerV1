369b1fd9a1f83ddc5a6741cce8d1a99aa346c38a
Leonardo Menezes <145611761+leonardomenezes7@users.noreply.github.com>
2023-12-15 05:05:48 -0400

    Add files via upload

---
+++ src/toggle-mode.js
@@ -0,0 +1,12 @@
+let darkMode = true
+const buttonToggle = document.getElementById('toggle-mode')
+
+buttonToggle.addEventListener('click', (event) => {
+  document.documentElement.classList.toggle('light')
+  
+  const mode = darkMode ? 'light' : 'dark'
+
+  event.currentTarget.querySelector('span').textContent = `${mode} ativado`
+
+  darkMode = !darkMode
+})
